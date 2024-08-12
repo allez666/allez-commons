e87add6c0ec8af8a0913f1c5fdb33a942b4e75e1
chenyu <401434187@qq.com>
2024-08-13 00:47:50 +0800

    临时提交

---
+++ allez-commons-starter/allez-commons-starter-redis/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author: chenGuanXi
+ * @create: 2024-08-13 00:46
+ * @Description: 模块写redis的一些公用操作、redis数据源管理、redisson分布式锁(声明式、编程式、lambda方式)...
+ */
+module allez.commons.starter.redis {
+}
