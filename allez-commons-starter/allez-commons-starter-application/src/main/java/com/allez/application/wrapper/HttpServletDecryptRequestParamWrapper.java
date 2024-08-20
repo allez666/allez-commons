9f132d7c77cd5c2cc828a0f05244253ba910f21c
chenyu <401434187@qq.com>
2024-08-20 23:55:24 +0800

    临时提交

--- allez-commons-starter/allez-commons-starter-application/src/main/java/com/allez/application/wrapper/HttpServletDecryptRequestParamWrapper.java
+++ allez-commons-starter/allez-commons-starter-application/src/main/java/com/allez/application/wrapper/HttpServletDecryptRequestParamWrapper.java
@@ -1,11 +1,17 @@
 package com.allez.application.wrapper;
 
+import com.allez.application.filter.RequestParamDecryptFilter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author chenyu
@@ -14,9 +20,9 @@ import java.util.Map;
  */
 public class HttpServletDecryptRequestParamWrapper extends GlobalHttpServletRequestWrapper {
 
-    private final Map<String,String> headerMap;
+    private final Map<String, String> headerMap;
 
-    private final Map<String,String[]> paramMap;
+    private final Map<String, String[]> paramMap;
 
     private final Collection<Part> parts;
 
@@ -31,6 +37,8 @@ public class HttpServletDecryptRequestParamWrapper extends GlobalHttpServletRequ
     public HttpServletDecryptRequestParamWrapper(HttpServletRequest request) {
         super(request);
         try {
+            this.headerMap = new HashMap<>();
+            this.paramMap = decryptParamMap(request);
             this.parts = request.getParts();
             this.urlQueryString = request.getQueryString();
         } catch (IOException | ServletException e) {
@@ -53,5 +61,32 @@ public class HttpServletDecryptRequestParamWrapper extends GlobalHttpServletRequ
         return this.parts;
     }
 
+
+    private Map<String, String[]> decryptParamMap(HttpServletRequest request) {
+        Map<String, String[]> resultMap = new HashMap<>();
+        Map<String, String[]> parameterMap = request.getParameterMap();
+
+        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+            String key = entry.getKey();
+            String[] value = entry.getValue();
+
+            try {
+                String decryptKey = RequestParamDecryptFilter.decrypt(key);
+                if (Objects.nonNull(value)) {
+                    String[] decryptValue = new String[value.length];
+                    for (int i = 0; i < value.length; i++) {
+                        decryptValue[i] = RequestParamDecryptFilter.decrypt(URLDecoder.decode(value[i], StandardCharsets.UTF_8));
+                    }
+                    resultMap.put(decryptKey, decryptValue);
+                }
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        return resultMap;
+    }
 
 }
