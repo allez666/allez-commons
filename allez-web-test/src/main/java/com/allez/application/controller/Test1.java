5ddd4aadf996d2270036e053ba1b7d4dccc60aa1
ChenYu <401434187@qq.com>
2025-12-05 15:14:34 +0800

    feat：测试类

---
+++ allez-web-test/src/main/java/com/allez/application/controller/Test1.java
@@ -0,0 +1,9 @@
+package com.allez.application.controller;
+
+/**
+ * @author ChenYu
+ * @date 2025-12-05 15:14:23
+ * @Description:
+ */
+public class Test1 {
+}
---
+++ allez-web-test/src/main/java/com/allez/application/controller/Test2.java
@@ -0,0 +1,9 @@
+package com.allez.application.controller;
+
+/**
+ * @author ChenYu
+ * @date 2025-12-05 15:14:27
+ * @Description:
+ */
+public class Test2 {
+}
