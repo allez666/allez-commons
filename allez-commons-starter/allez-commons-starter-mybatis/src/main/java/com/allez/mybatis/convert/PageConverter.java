76ac18f0f6c3508217611146821147c6e25cdf52
ChenYu <123@qq.com>
2026-03-01 02:14:54 +0800

    feat：临时提交

--- allez-commons-starter/allez-commons-starter-mybatis/src/main/java/com/allez/mybatis/convert/PageConverter.java
+++ allez-commons-starter/allez-commons-starter-mybatis/src/main/java/com/allez/mybatis/convert/PageConverter.java
@@ -1,7 +1,14 @@
 package com.allez.mybatis.convert;
 
 import com.allez.lang.entity.PageResponse;
+import com.allez.lang.entity.Sort;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class PageConverter {
     private PageConverter() {
@@ -13,6 +20,32 @@ public final class PageConverter {
                 (int) page.getCurrent(), (int) page.getSize()
                 , page.getTotal(), page.getRecords()
         );
+    }
+
+    /**
+     * 将 Sort 对象转换为 MyBatis-Plus 的 OrderItem 列表
+     *
+     * @param sort 业务排序对象
+     * @return MP 排序项列表，若为空则返回空列表
+     */
+    public static List<OrderItem> convert(Sort sort) {
+        if (sort == null || CollectionUtils.isEmpty(sort.getOrderChains())) {
+            return Collections.emptyList();
+        }
+
+        return sort.getOrderChains()
+                .stream()
+                .map(PageConverter::adaptOrderChain)
+                .collect(Collectors.toList());
+    }
+
+
+    private static OrderItem adaptOrderChain(Sort.OrderItem orderItem) {
+        if (orderItem.getDirection().isAscending()) {
+            return OrderItem.asc(orderItem.getProperty());
+        } else {
+            return OrderItem.desc(orderItem.getProperty());
+        }
     }
 
 }
--- allez-commons-starter/allez-commons-starter-mybatis/src/main/java/com/allez/mybatis/dao/BaseDao.java
+++ allez-commons-starter/allez-commons-starter-mybatis/src/main/java/com/allez/mybatis/dao/BaseDao.java
@@ -3,19 +3,13 @@ package com.allez.mybatis.dao;
 import com.allez.lang.entity.BaseDo;
 import com.allez.lang.entity.PageRequest;
 import com.allez.lang.entity.PageResponse;
+import com.allez.lang.entity.Sort;
 import com.allez.mybatis.convert.PageConverter;
-import com.allez.mybatis.convert.SortConverter;
-import com.allez.mybatis.test.TestEntity;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
-import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.apache.ibatis.session.ExecutorType;
-import org.apache.ibatis.session.SqlSession;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -31,21 +25,31 @@ import java.util.function.Function;
 public abstract class BaseDao<M extends BaseMapper<T>, T extends BaseDo<ID>, ID extends Serializable>
         extends ServiceImpl<M, T> {
 
-    public <R> PageResponse<R> page(PageRequest req, Consumer<LambdaQueryWrapper<T>> consumer, Function<T, R> mapper) {
-        Page<T> pageReq = Page.of(req.getPageNumber(), req.getPageSize());
-        pageReq.addOrder(SortConverter.convert(req.getSort()));
 
+    public PageResponse<T> page(PageRequest req, Consumer<LambdaQueryWrapper<T>> consumer) {
+        return this.page(req, consumer, Function.identity());
+    }
+
+    public <R> PageResponse<R> page(PageRequest req, Consumer<LambdaQueryWrapper<T>> consumer, Function<T, R> mapper) {
+
+        // page
+        Page<T> pageReq = Page.of(req.getPageNumber(), req.getPageSize());
+
+        // sort
+        Sort sort = req.getSort();
+        pageReq.addOrder(PageConverter.convert(sort));
+
+        // condition
         LambdaQueryWrapper<T> lambdaQueryWrapper = new LambdaQueryWrapper<>();
         consumer.accept(lambdaQueryWrapper);
 
-        super.executeBatch(new ArrayList<>(),((sqlSession, o) -> {
+        IPage<T> page = super.page(pageReq, lambdaQueryWrapper);
 
-        }));
-
-        IPage<T> page = super.page(pageReq,lambdaQueryWrapper);
+        super.executeBatch()
         return PageConverter.convert(page)
                 .mapToPage(mapper);
     }
+
 
 
 }
